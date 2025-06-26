3a564969dc8b1da01858afe40ca9dc4f6ff39799
♦ Venen Jean ♦ <102526504+VenenJean@users.noreply.github.com>
2025-06-26 07:43:45 +0200

    Rollback

--- scripts/main.js
+++ scripts/main.js
@@ -16,20 +16,12 @@ function printDocument() {
     const form = document.getElementById('lieferschein-form');
 
     if (form.checkValidity()) {
-        const wrapper = document.getElementById("print-wrapper");
-        const original = document.getElementById("original-content");
-        const clone = original.cloneNode(true);
-
-        wrapper.innerHTML = '';
-        wrapper.appendChild(original);
-        wrapper.appendChild(clone);
-
+        alert("Bitte im Druckdialog die Anzahl der Kopien auf 2 setzen!");
         window.print();
     } else {
         form.reportValidity();
     }
 }
-
 
 function updateAmount() {
     const packageInput = document.getElementById("package-count");
